0d27426f826ba629807d7940fe1c0febfe39f84c
Elmer Salazar <huntergps79@yahoo.es>
2024-12-05 13:30:21 -0500

    Solcuionar referencia cruzada

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from . import res_config_settings
 from . import pos_session
 from . import pos_order
 from . import pos_order_line
-from . import res_config_settings
 from . import res_company
